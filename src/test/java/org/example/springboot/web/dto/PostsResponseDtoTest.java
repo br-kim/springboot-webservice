9d517d972b9d37da920b035f614b53895c09bc34
idle947 <idle947@gmail.com>
2020-07-19 21:34:17 +0900

    PostsResponseDtoTest 작성

---
+++ src/test/java/org/example/springboot/web/dto/PostsResponseDtoTest.java
@@ -0,0 +1,32 @@
+package org.example.springboot.web.dto;
+
+import org.example.springboot.domain.posts.Posts;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PostsResponseDtoTest {
+
+    @Test
+    public void 게시글_DTO_테스트(){
+        //given
+        String title = "제목";
+        String content = "내용";
+        String author = "idle947@gmail.com";
+
+        //when
+        Posts entity = Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+
+        PostsResponseDto postsResponseDto = new PostsResponseDto(entity);
+
+        //then
+        assertThat(postsResponseDto.getTitle()).isEqualTo(title);
+        assertThat(postsResponseDto.getContent()).isEqualTo(content);
+        assertThat(postsResponseDto.getAuthor()).isEqualTo(author);
+    }
+
+}
